eb7f492b8f6e5b143201c1ab4a6aa1b08b3a0d33
adichowdhuri <adichowdhuri@gmail.com>
2023-10-24 09:58:44 +0530

    fixed upload io stream issue

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import os
 import io
 import numpy as np
-from keras.models import load_model
 from keras.applications.vgg16 import VGG16
 from keras.layers import Input, Lambda, Dense, Flatten
 from keras.applications.vgg16 import preprocess_input
@@ -22,9 +21,9 @@ x = Flatten()(vgg.output)
 prediction = Dense(2, activation='softmax')(x)
 model = Model(inputs=vgg.input, outputs=prediction)
 
-model.load_weights('training_1/cp.ckpt')
+model.load_weights('training_1\cp.ckpt')
 # List all subdirectories (bird name folders) in the testing directory
-bird_name_folders = ["Normal", 'Pneumonia']
+folders = ["Normal", 'Pneumonia']
 
 # Streamlit app
 st.title("Pneumonia Detecter")
@@ -33,21 +32,22 @@ st.title("Pneumonia Detecter")
 uploaded_image = st.file_uploader("Upload an Xray", type=["jpg", "jpeg", "png"])
 
 if uploaded_image is not None:
+    image_stream = io.BytesIO(uploaded_image.getvalue())
+
     # Display the uploaded image
     st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
 
     # Make predictions on the uploaded image
-    img = image.load_img(uploaded_image, target_size=(224, 224))
+    img = image.load_img(image_stream, target_size=(224, 224))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
 
-    img = np.expand_dims(img, axis=0)
     img = preprocess_input(img)
     prediction = model.predict(img)
     predicted_class_index = np.argmax(prediction[0])
 
     # Get the predicted class name directly from the folder name
-    predicted_class_name = bird_name_folders[predicted_class_index]
+    predicted_class_name = folders[predicted_class_index]
 
     # Display the prediction
     st.title(f"Diagnosis: {predicted_class_name}")
